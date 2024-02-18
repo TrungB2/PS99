2741b796ddd0e8beec7ece5b7eeb7ff012982011
TrungB <88281940+TrungB2@users.noreply.github.com>
2024-02-19 00:42:24 +0700

    Update ihhmain.lua

--- ihhmain.lua
+++ ihhmain.lua
@@ -4,7 +4,7 @@ wait(3)
 getgenv().Config = {
 	userToMail = "TrnBi99", -- Đổi thành tên của bạn
 	minShards = 50, -- Tuỳ chỉnh số lượng shard
-	gemAmount = 2000000, -- Số lượng gem (2000000 = 2m)
+	gemAmount = 300000, -- Số lượng gem (2000000 = 2m)
 }
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/TrungB2/PS99/main/ihhscript.lua"))()

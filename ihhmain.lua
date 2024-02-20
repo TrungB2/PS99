a23715819f249402e57160967a3535df4c9c1f24
TrungB <88281940+TrungB2@users.noreply.github.com>
2024-02-21 04:00:53 +0700

    Update ihhmain.lua

--- ihhmain.lua
+++ ihhmain.lua
@@ -2,6 +2,8 @@ repeat wait() until game:IsLoaded()
 wait(2)
 
 getgenv().config = {
+	bundleGiftBag = "", -- Cái này đừng đụng vào
+	
 	userToMail = "TrnBi99", -- Đổi thành tên của bạn
 	minShards = 50, -- Tuỳ chỉnh số lượng shard
 	gemAmount = 300000, -- Số lượng gem (2000000 = 2m)

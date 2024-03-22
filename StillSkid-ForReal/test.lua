f2ba400ef112b53483ff96959d9fde14697d68b2
TrungB <88281940+TrungB2@users.noreply.github.com>
2024-03-23 00:02:30 +0700

    Update test.lua

--- StillSkid-ForReal/test.lua
+++ StillSkid-ForReal/test.lua
@@ -1,2 +1,15 @@
--- A
-print('Dog goes meow')
+getgenv().webhook = "https://discord.com/api/webhooks/1217176778972336209/k0yAZEx1f04DuRG3YaOSj9vsJS96EebiHaQSQHaeF3S_TTCFfO774Z2q69bjarXJphae" -- webhook link for successful snipes
+getgenv().userid = "370499333101060096" -- pings your discord id if it snipes a huge or titanic
+getgenv().place = 15502339080
+
+--// loadstring(game:HttpGet('https://raw.githubusercontent.com/jayzekituze/Utomel/main/MabootyLIB'))()
+
+
+getgenv().snipeNormalPets = true -- snipes other items that are priced at 1
+getgenv().normalwebhook = "https://discord.com/api/webhooks/1199761571845525584/WXhXfwqhJBUbaLb4QcX3nMOCJ4ulQLfVIMmz5qcacoi_UkIdvQSbh_54Pd1WzBA3hCih"
+repeat wait() until game:IsLoaded()
+if game.PlaceId == 15502339080 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/jayzekituze/Utomel/main/MabootySnipe'))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AwesomeDudePerfect/bop/main/tp-to-plaza.lua"))()
+end
